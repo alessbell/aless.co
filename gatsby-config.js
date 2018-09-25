a054cb2c379e2d44106b0cf007a16df89713b53a
Alessia Bellisario <bellisario.alessia@gmail.com>
2018-09-24 22:00:55 -0400

    Remove pathprefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,5 @@ module.exports = {
   siteMetadata: {
     title: `alessia bellisario`,
   },
-  pathPrefix: "/alessbell",
   plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-emotion`, `gatsby-plugin-flow`],
 }

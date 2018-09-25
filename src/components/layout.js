8fe7c66274de8cee71c853325b0a7f8345817684
Alessia Bellisario <bellisario.alessia@gmail.com>
2018-09-24 22:19:19 -0400

    fix border

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,41 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import styled from 'react-emotion';
 
 import '../layouts/font-face.css'
 import '../layouts/index.css'
-
-const BaseBorder = styled.div`
-  background: red;
-  position: fixed;
-  z-index: 101;
-`;
-
-const LeftRightSide = styled(BaseBorder)`
-  left: 0;
-  right: 0;
-  width: 3px;
-`;
-const TopBottomSide = styled(BaseBorder)`
-  top: 0;
-  bottom: 0;
-  width: 3px;
-`;
-
-const Left = styled(LeftRightSide)`
-  left: 0;
-`;
-const Right = styled(LeftRightSide)`
-  right: 0'
-`;
-
-const Top = styled(TopBottomSide)`
-  top: 0;
-`;
-const Bottom = styled(TopBottomSide)`
-  top: 0;
-`;
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -50,10 +18,10 @@ const TemplateWrapper = ({ children }) => (
     >
       <html lang="en" />
     </Helmet>
-    <Left />
-    <Right />
-    <Top />
-    <Bottom />
+    <div id="top" />
+    <div id="bottom" />
+    <div id="left" />
+    <div id="right" />
     {children}
   </div>
 )

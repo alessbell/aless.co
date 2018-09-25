b74ee14a0ec20fb01cee9d0f891a27d3381ab012
Alessia Bellisario <bellisario.alessia@gmail.com>
2018-09-24 21:49:22 -0400

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,6 @@ module.exports = {
   siteMetadata: {
     title: `alessia bellisario`,
   },
+  pathPrefix: "/alessbell",
   plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-emotion`, `gatsby-plugin-flow`],
 }

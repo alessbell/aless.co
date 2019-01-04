608e84628333bbc9c4a093d63595442dbffd28d2
Alessia Bellisario <bellisario.alessia@gmail.com>
2019-01-04 02:28:04 +0000

    Updates dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: `alessia bellisario`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-emotion`, `gatsby-plugin-flow`],
-}
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-flow`,
+  ],
+};

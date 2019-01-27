50d81de8ef6b3571a58f66b93845ef3b7c1be922
Alessia Bellisario <alessia@breather.com>
2019-01-27 16:44:53 -0500

    First post

--- content/blog/introducing-anti-pattern/index.md
+++
@@ -1,9 +0,0 @@
----
-title: Introducing Anti Pattern
-date: '2019-01-27T16:19:15.020Z'
-spoiler: It's not what you think
----
-
-This is an MDX page.
-
-![Recurse Center Logo](./1.jpg 'Recurse Center Logo')
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,23 +4,22 @@ import Layout from '../components/layout';
 const AboutPage = () => (
   <Layout>
     <h1>about</h1>
-    <p>Hello, I'm Alessia Bellisario. I live and work in New York City.</p>
+    <p>Hi, I'm Alessia Bellisario. I live in New York City.</p>
     <p>
       I work at{' '}
       <a href="https://breather.com" target="_blank" rel="noopener noreferrer">
         Breather
       </a>{' '}
       as a full stack web engineer, and in April I'll spend a week at the{' '}
-      <a
-        href="https://http://recurse.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a href="https://recurse.com/" target="_blank" rel="noopener noreferrer">
         Recurse Center
       </a>{' '}
       as a member of the mini 3 2019 batch.
     </p>
-    <p>This blog will mostly explore my interests </p>
+    <p>
+      This blog will explore topics in JavaScript, React, and maybe even Rust,
+      Wasm, WebGL, generative art... stay tuned ¯\_(ツ)_/¯
+    </p>
   </Layout>
 );
 

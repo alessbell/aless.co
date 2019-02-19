c16ec00eccda378c754fcbbb35ba22e9c0d52f59
Alessia Bellisario <github@bellisar.io>
2019-02-19 12:50:45 -0500

    Adds RSS feed (#31)
    * Adds RSS feed
    
    * Fix title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: `anti/pattern`,
-    author: `alessia bellisario`,
+    author: `Alessia Bellisario`,
     siteUrl: 'https://aless.co',
-    description: `alessia bellisario is a programmer working on the web in New York City`,
+    description: `Alessia Bellisario is a programmer working on the web in New York City`,
   },
   plugins: [
     {
@@ -55,13 +55,67 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `anti/pattern: alessia's blog`,
+        name: `anti/pattern: a blog by alessia bellisario`,
         short_name: `alessia's blog`,
         start_url: '/',
         background_color: '#fff',
         theme_color: '#525dce',
         display: 'standalone',
         icon: 'static/favicon-32x32.png',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                });
+              });
+            },
+            query: `
+            {
+                allMdx(limit: 1000, sort: { fields: [frontmatter___date], order: DESC }) {
+                  edges {
+                    node {
+                      html
+                      excerpt
+                      fields {
+                        slug
+                      }
+                      frontmatter {
+                        date(formatString: "MMMM DD, YYYY")
+                        spoiler
+                        title
+                      }
+                    }
+                  }
+                }
+              }
+          `,
+            output: '/rss.xml',
+            title: 'anti/pattern: a blog by alessia bellisario',
+          },
+        ],
       },
     },
     `gatsby-plugin-sitemap`,

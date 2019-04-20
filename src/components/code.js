531a62728a24d1534194b4853079ae1674aa1828
Alessia Bellisario <alessia@breather.com>
2019-04-20 16:10:15 -0400

    Upgrades mdx and code component

--- src/components/code.js
+++ src/components/code.js
@@ -3,35 +3,55 @@ import Highlight, { defaultProps } from 'prism-react-renderer';
 import Theme from 'prism-react-renderer/themes/nightOwl';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 
-export const Code = ({ codeString, language, ...props }) => {
-  if (props['react-live']) {
+export default ({ children, className, live, render }) => {
+  const language = className.replace(/language-/, '');
+
+  if (live) {
     return (
-      <LiveProvider code={codeString} noInline={true}>
-        <LiveEditor />
-        <LiveError />
-        <LivePreview />
-      </LiveProvider>
-    );
-  } else {
-    return (
-      <Highlight
-        {...defaultProps}
-        theme={Theme}
-        code={codeString}
-        language={language}
-      >
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={className} style={style}>
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
-          </pre>
-        )}
-      </Highlight>
+      <div style={{ backgroundColor: 'black' }}>
+        <LiveProvider code={children.trim()}>
+          <LivePreview />
+          <LiveEditor />
+          <LiveError />
+        </LiveProvider>
+      </div>
     );
   }
+
+  if (render) {
+    return (
+      <div>
+        <LiveProvider code={children}>
+          <LivePreview />
+        </LiveProvider>
+      </div>
+    );
+  }
+
+  return (
+    <Highlight
+      {...defaultProps}
+      theme={Theme}
+      code={children}
+      language={language}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, padding: '20px' }}>
+          {/* render null for last empty line because
+          it was adding a span that looked like padding */}
+          {tokens.map((line, i) => {
+            return i + 1 === tokens.length &&
+              line.length === 1 &&
+              line[0].empty ? null : (
+              <div key={i} {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            );
+          })}
+        </pre>
+      )}
+    </Highlight>
+  );
 };
--- wrap-root-element.js
+++ wrap-root-element.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { MDXProvider } from '@mdx-js/tag';
-import { Code } from './src/components/code';
-import { preToCodeBlock } from 'mdx-utils';
+import { MDXProvider } from '@mdx-js/react';
+import Code from './src/components/code';
 
-// components is its own object outside of render so that the references to
-// components are stable
 const components = {
-  pre: preProps => {
-    const props = preToCodeBlock(preProps);
-    // if there's a codeString and some props, we passed the test
-    if (props) {
-      return <Code {...props} />;
-    } else {
-      // it's possible to have a pre without a code in it
-      return <pre {...preProps} />;
-    }
-  },
-};
+  pre: props => <div {...props} />,
+  code: props => <Code {...props} />
+}
+
 export const wrapRootElement = ({ element }) => (
   <MDXProvider components={components}>{element}</MDXProvider>
 );

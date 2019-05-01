4920a0875af2c3efb6749af91db64d7c253249ed
Alessia Bellisario <alessia@breather.com>
2019-05-01 19:02:30 -0400

    Remove post and fix header

--- src/components/header.js
+++ src/components/header.js
@@ -10,26 +10,12 @@ class Header extends React.Component {
     this.setState({ initialLoad: true });
     window.onresize = this.onresize;
   }
-  onresize = () => {
-    const title = document.getElementById('title');
-    if (title) {
-      this.setState({ boundingRect: title.getBoundingClientRect() });
-    }
-  };
   render() {
     const { initialLoad } = this.state;
-    let titleEl;
-    let boundingRect;
-    if (typeof document !== `undefined`) {
-      titleEl = document.getElementById('title');
-    }
-    if (titleEl) {
-      boundingRect = this.state.boundingRect || titleEl.getBoundingClientRect();
-    }
     return (
       initialLoad && (
         <div style={{ position: 'absolute', top: '0' }}>
-          <Surface width={boundingRect.width} height={boundingRect.height - 2}>
+          <Surface width={270} height={42}>
             <HelloBlue />
           </Surface>
         </div>

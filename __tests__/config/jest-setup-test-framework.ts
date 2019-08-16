fc5849529079e1c7bdb9475f51dddaf4a881a56c
Alessia Bellisario <github@bellisar.io>
2019-08-16 11:05:13 -0400

    Adds ogImage (#334)

--- __tests__/config/jest-setup-test-framework.ts
+++ __tests__/config/jest-setup-test-framework.ts
@@ -10,6 +10,14 @@ beforeEach(() => {
         title: `anti/pattern`,
         repository: `https://github.com/alessbell/alessbell`,
         commit: `master`,
+        siteUrl: `https://aless.co`,
+      },
+    },
+    ogImageDefault: {
+      childImageSharp: {
+        fixed: {
+          src: 'https://aless.co/assets/og-image.png',
+        },
       },
     },
   }));

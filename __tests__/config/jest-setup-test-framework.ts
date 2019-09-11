fa5641c7b5dfaaedda2a0738139f8c888c276698
Alessia Bellisario <github@bellisar.io>
2019-09-10 21:24:50 -0400

    Fix intermittent test failure by mocking Header

--- __tests__/config/jest-setup-test-framework.ts
+++ __tests__/config/jest-setup-test-framework.ts
@@ -3,6 +3,14 @@ import { useStaticQuery } from 'gatsby';
 
 jest.mock('gl-react-dom');
 
+// must return object with default key because of issue with how
+// import/export behaves in TS compared to ES6
+// see: https://github.com/kulshekhar/ts-jest/issues/120#issuecomment-283653644
+
+jest.mock('../../src/components/header', () => ({
+  default: () => 'AnimatedColorWave',
+}));
+
 beforeEach(() => {
   (useStaticQuery as jest.Mock).mockImplementation(() => ({
     site: {

f2f020a94b5fe9545ab533ac3fe91e2998964b35
Alessia Bellisario <alessia@breather.com>
2019-10-04 08:44:56 -0400

    Test new file push

--- open-resume-pr/entrypoint.sh
+++ open-resume-pr/entrypoint.sh
@@ -7,7 +7,22 @@ main() {
   echo 'Git version:' $(git --version)
   echo 'output of pwd:' && pwd
   echo 'output of ls:' && ls
+  echo 'output of git config:' && git config --list
   echo 'these are arguments set to the workflow' && echo ${*}
+
+  # get latest release of alessbell/resume using GET /repos/:owner/:repo/releases/latest
+  # see: https://developer.github.com/v3/repos/releases/#list-releases-for-a-repository
+  # GET https://api.github.com/repos/alessbell/resume/releases/latest
+
+  # download resume.pdf and save in static/resume.pdf
+  touch newfile.js
+
+  # git add and push to branch beginning with resume/
+  git config --global push.default current
+  git checkout -b resume/new-version
+  git add .
+  git commit -m "Trying something"
+  git push -u
 }
 
 main

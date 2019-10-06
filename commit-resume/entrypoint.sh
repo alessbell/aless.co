17e1205b025603df8cd759638958e1cfa192bcce
Alessia Bellisario <alessia@breather.com>
2019-10-06 12:03:34 -0400

    Fix jq command

--- commit-resume/entrypoint.sh
+++ commit-resume/entrypoint.sh
@@ -68,7 +68,7 @@ main() {
   RELEASES_URL=https://api.github.com/repos/alessbell/resume/releases/latest
 
   RES=$(curl -sSL -H "${AUTH_HEADER}" -H "${HEADER}" --user "${GITHUB_ACTOR}" -X GET ${RELEASES_URL})
-  VERSION=$(echo "${RES}" | jq --raw-output '.[] | .tag_name')
+  VERSION=$(echo "${RES}" | jq --raw-output '.tag_name')
   PDF_URL="https://github.com/alessbell/resume/releases/download/${VERSION}/resume.pdf"
   echo "PDF URL: ${PDF_URL}"
 

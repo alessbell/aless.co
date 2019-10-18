da3a0c9704fb51269597a9090a2d4827980fede3
Alessia Bellisario <alessia@breather.com>
2019-10-18 10:10:10 -0400

    Adds shader

--- src/components/colorwave.tsx
+++ src/components/colorwave.tsx
@@ -13,6 +13,30 @@ const shaders = Shaders.create({
   },
 });
 
+const shaders2 = Shaders.create({
+  gradients: {
+    frag: GLSL`
+    precision highp float;
+    varying vec2 uv;
+    uniform vec4 colors[3];
+    uniform vec2 particles[3];
+    void main () {
+      vec4 sum = vec4(0.0);
+      for (int i=0; i<3; i++) {
+        vec4 c = colors[i];
+        vec2 p = particles[i];
+        float d = c.a * smoothstep(0.6, 0.2, distance(p, uv));
+        sum += d * vec4(c.a * c.rgb, c.a);
+      }
+      if (sum.a > 1.0) {
+        sum.rgb /= sum.a;
+        sum.a = 1.0;
+      }
+      gl_FragColor = vec4(sum.a * sum.rgb, 1.0);
+    }`,
+  },
+});
+
 export interface ColorWaveProps {
   blue: number;
 }
@@ -22,3 +46,17 @@ const ColorWave: React.FunctionComponent<ColorWaveProps> = ({ blue }) => (
 );
 
 export default ColorWave;
+
+export const OtherColorWave: React.FunctionComponent<{}> = () => (
+  <Node
+    shader={shaders2 && shaders2.gradients}
+    uniforms={{
+      colors: [
+        [Math.cos(0.002), Math.sin(0.002), 0.2, 1],
+        [Math.sin(0.002), -Math.cos(0.002), 0.1, 1],
+        [0.3, Math.sin(3 + 0.002), Math.cos(1 + 0.003), 1],
+      ],
+      particles: [[0.3, 0.3], [0.7, 0.5], [0.4, 0.9]],
+    }}
+  />
+);
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
 import { Surface } from 'gl-react-dom';
 import AnimatedColorWave from './animated';
+import { OtherColorWave } from './colorwave';
 
 const mixBlendMode = { mixBlendMode: `var(--mixBlendMode2)` } as any;
 
-const Header: React.FunctionComponent = () => (
-  <div style={{ position: 'absolute', top: '0', ...mixBlendMode }}>
-    <span style={{ position: 'absolute', top: '0', ...mixBlendMode }}>
-      <Surface width={253} height={47}>
-        <AnimatedColorWave />
-      </Surface>
-    </span>
-  </div>
-);
+const Header: React.FunctionComponent = () => {
+  const [shader, setShader] = React.useState(true);
+  return (
+    <div style={{ position: 'absolute', top: '0', ...mixBlendMode }}>
+      <span style={{ position: 'absolute', top: '0', ...mixBlendMode }}>
+        <Surface width={253} height={47}>
+          {shader ? <AnimatedColorWave /> : <OtherColorWave />}
+        </Surface>
+      </span>
+      <button onClick={() => setShader(!shader)}>other shader</button>
+    </div>
+  );
+};
 
 export default Header;

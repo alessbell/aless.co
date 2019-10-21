91aef1d68e142928e8baac365bcb053315ca6393
Alessia Bellisario <alessia@breather.com>
2019-10-20 23:46:55 -0400

    Fix ssr again

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,21 +2,16 @@ import * as React from 'react';
 import { Surface } from 'gl-react-dom';
 import AnimatedColorWave from './animated';
 
-const Header: React.FunctionComponent = () => {
-  const mixBlendMode = { mixBlendMode: `var(--mixBlendMode2)` } as any;
-  return (
-    <div
-      style={{
-        position: 'absolute',
-        top: '0',
-        ...mixBlendMode,
-      }}
-    >
+const mixBlendMode = { mixBlendMode: `var(--mixBlendMode2)` } as any;
+
+const Header: React.FunctionComponent = () => (
+  <div style={{ position: 'absolute', top: '0', ...mixBlendMode }}>
+    <span style={{ position: 'absolute', top: '0', ...mixBlendMode }}>
       <Surface width={253} height={47}>
         <AnimatedColorWave />
       </Surface>
-    </div>
-  );
-};
+    </span>
+  </div>
+);
 
 export default Header;

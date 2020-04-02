21824e6be1b02e38122e844fba32116322b9de41
Alessia Bellisario <alessia@breather.com>
2020-04-02 13:39:51 -0400

    More WIP

--- src/css.d.ts
+++
@@ -1,8 +0,0 @@
-import CSS from 'csstype';
-
-declare module 'csstype' {
-  interface Properties {
-    // Add a CSS Custom Property
-    mixBlendMode?: CSS.MixBlendModeProperty | 'var(--mixBlendMode2)';
-  }
-}

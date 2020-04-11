92888874351b1ef2c3aeff83b5fa00375dd37486
Alessia Bellisario <github@bellisar.io>
2020-04-11 10:44:40 -0400

    New react-helmet API

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
 export interface SEOProps {

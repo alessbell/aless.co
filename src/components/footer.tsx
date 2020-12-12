9be68fdb5c5b38caa556bd652027ad31c6272fc1
Alessia Bellisario <github@bellisar.io>
2020-12-12 10:04:54 -0500

    No brackets needed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -34,74 +34,72 @@ const Footer = ({
 }: {
   commit?: string | null;
   repository?: string | null;
-}): JSX.Element => {
-  return (
-    <FooterWrapper>
-      <Link to="/about/">about</Link>
+}): JSX.Element => (
+  <FooterWrapper>
+    <Link to="/about/">about</Link>
 
-      <Space color="orange">•</Space>
+    <Space color="orange">•</Space>
 
-      <Anchor
-        link="https://github.com/alessbell"
+    <Anchor
+      link="https://github.com/alessbell"
+      target="_blank"
+      rel="noopener"
+      name="github"
+    />
+
+    <Space color="red">•</Space>
+
+    <Anchor
+      link="https://twitter.com/alessbell"
+      target="_blank"
+      rel="noopener"
+      name="twitter"
+    />
+
+    <Space color="green">•</Space>
+
+    <Anchor link="/resume.pdf" name="resume" />
+
+    <Space color="pink">•</Space>
+
+    <a href="/rss.xml">rss</a>
+
+    {commit && repository ? (
+      <Commit>
+        deployed commit:{' '}
+        <code>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={`${repository}/commit/${commit}`}
+          >
+            {commit?.substring(0, 7)}
+          </a>
+        </code>
+      </Commit>
+    ) : null}
+    <div className="webrings">
+      <a
+        className="webring-link"
+        href="https://webring.xxiivv.com/#random"
         target="_blank"
-        rel="noopener"
-        name="github"
-      />
-
-      <Space color="red">•</Space>
-
-      <Anchor
-        link="https://twitter.com/alessbell"
+        rel="noreferrer"
+      >
+        <img
+          src={iconBlack as string}
+          style={{ width: '55px', marginRight: '4px', marginBottom: '-9px' }}
+        />
+      </a>
+      <a
+        className="webring-link"
+        href="https://webring.recurse.com"
         target="_blank"
-        rel="noopener"
-        name="twitter"
-      />
-
-      <Space color="green">•</Space>
-
-      <Anchor link="/resume.pdf" name="resume" />
-
-      <Space color="pink">•</Space>
-
-      <a href="/rss.xml">rss</a>
-
-      {commit && repository ? (
-        <Commit>
-          deployed commit:{' '}
-          <code>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={`${repository}/commit/${commit}`}
-            >
-              {commit?.substring(0, 7)}
-            </a>
-          </code>
-        </Commit>
-      ) : null}
-      <div className="webrings">
-        <a
-          className="webring-link"
-          href="https://webring.xxiivv.com/#random"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={iconBlack as string}
-            style={{ width: '55px', marginRight: '4px', marginBottom: '-9px' }}
-          />
-        </a>
-        <a
-          className="webring-link"
-          href="https://webring.recurse.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={rcLogo as string} style={{ width: '36px' }} />
-        </a>
-      </div>
-    </FooterWrapper>
-  );
-};
+        rel="noreferrer"
+      >
+        <img src={rcLogo as string} style={{ width: '36px' }} />
+      </a>
+    </div>
+  </FooterWrapper>
+);
 
 export default Footer;

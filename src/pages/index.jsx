4f4b417b042c847ebbfec112dbc8d70e599dd469
alessia <alessia@apollographql.com>
2022-10-15 00:23:15 -0400

    fix: use anchor tag for resume link

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -205,6 +205,7 @@ function Resume() {
         href="/resume.pdf"
         variant="secondary"
         className="group mt-6 w-full"
+        absolute
       >
         Download Resume
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />

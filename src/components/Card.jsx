629fa754705cb6686e1fa3337b979eb9114cfaac
alessia <alessia@apollographql.com>
2023-01-23 22:37:20 -0500

    fix borders

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -27,7 +27,7 @@ export function Card({ as: Component = 'div', className, children }) {
 Card.Link = function CardLink({ children, ...props }) {
   return (
     <>
-      <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 group-hover:border border-2 dark:border-emerald-200/50 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl" />
+      <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition border-zinc-200/50 group-hover:scale-100 group-hover:opacity-100 group-hover:border border-2 dark:border-zinc-300/20 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl" />
       <Link {...props}>
         <span className="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl" />
         <span className="relative z-10">{children}</span>

54932b87d03b29225fade441ef22d6384278d5c5
alessia <alessia@apollographql.com>
2023-04-25 10:54:42 -0400

    update speaking page

--- src/pages/speaking.jsx
+++ src/pages/speaking.jsx
@@ -18,9 +18,9 @@ function Appearance({ title, description, event, cta, href }) {
       <Card.Title as="h3" href={href}>
         {title}
       </Card.Title>
-      <Card.Eyebrow decorate>{event}</Card.Eyebrow>
-      <Card.Description>{description}</Card.Description>
-      <Card.Cta>{cta}</Card.Cta>
+      {event && <Card.Eyebrow decorate>{event}</Card.Eyebrow>}
+      {description && <Card.Description>{description}</Card.Description>}
+      {cta && <Card.Cta>{cta}</Card.Cta>}
     </Card>
   )
 }
@@ -37,21 +37,28 @@ export default function Speaking() {
         intro="I'm looking for more opportunities to speak at tech events of all kinds this year - please get in touch if you happen to be organizing one!"
       >
         <div className="space-y-20">
-          <SpeakingSection title="Meetups">
+          <SpeakingSection title="Upcoming">
+            <Appearance
+              title="How to Use Suspense and GraphQL with Apollo to Build Great User Experiences"
+              event="React Advanced London (October 2023)"
+              description="The Apollo Client team will show you how we built a non-trivial app using Apollo Client’s new Suspense features and GraphQL features like the @defer directive."
+              href="https://reactadvanced.com/"
+              cta="View React Advanced site"
+            />
+          </SpeakingSection>
+          <SpeakingSection title="Past">
             <Appearance
               href="https://defer-presentation.vercel.app/"
               title="@defer all the (slow) things"
               description="An overview of GraphQL's proposed @defer directive, now available in Apollo Client and GraphOS."
-              event="ChicagoJS (2023)"
+              event="ChicagoJS (March 2023)"
               cta="View slides"
             />
-          </SpeakingSection>
-          <SpeakingSection title="Conferences">
             <Appearance
               href="https://youtu.be/tq9MfIG-VXw?t=14526"
               title="Next i18n: Performant, Rich Translations in Next.js"
               description="Inter­nation­ali­zation isn't a feature, it’s an architecture."
-              event="JS@PayPal Worldwide Virtual Conference (2021)"
+              event="JS@PayPal Worldwide Virtual Conference (May 2021)"
               cta="Watch video"
             />
             <Appearance

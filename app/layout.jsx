f9e0444315881658dea7fdd8ae89914cae70517a
alessia <alessia@apollographql.com>
2023-07-12 22:16:51 -0400

    fix hydration mismatch error

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,16 +12,18 @@ export default function RootLayout({ children }) {
   // )
   return (
     <html>
-      <div className="fixed inset-0 flex justify-center sm:px-8">
-        <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+      <body>
+        <div className="fixed inset-0 flex justify-center sm:px-8">
+          <div className="flex w-full max-w-7xl lg:px-8">
+            <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+          </div>
         </div>
-      </div>
-      <div className="relative">
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </div>
+        <div className="relative">
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </div>
+      </body>
     </html>
   )
 }

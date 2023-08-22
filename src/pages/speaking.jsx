200277f90c56f1a965f04894740dc0c04ab5352c
Alessia Bellisario <github@bellisar.io>
2023-08-22 14:53:55 -0600

    chore: update speaking page

--- src/pages/speaking.jsx
+++ src/pages/speaking.jsx
@@ -32,47 +32,50 @@ export default function Speaking() {
         <title>Speaking - Alessia Bellisario</title>
         <meta name="description" content="A few talks I've given" />
       </Head>
-      <SimpleLayout
-        title="A few talks I've given"
-        intro="I'm looking for more opportunities to speak at tech events of all kinds this year - please get in touch if you happen to be organizing one!"
-      >
+      <SimpleLayout title="A few talks I've given">
         <div className="space-y-20">
           <SpeakingSection title="Upcoming">
             <Appearance
-              title="Suspense + GraphQL = Superpowers"
-              event="React Rally (August 2023)"
-              // description="The Apollo Client team will show you how we built a non-trivial app using Apollo Client’s new Suspense features and GraphQL features like the @defer directive."
-              href="https://www.reactrally.com/speakers"
-              cta="View React Rally site"
+              title="@defer all the slow things"
+              event="GraphQL Conf 2023"
+              description="A lightning talk on GraphQL's proposed @defer directive."
+              href="https://graphql.org/conf/"
+              cta="View GraphQL Conf site"
+            />
+            <Appearance
+              title="Query Now, Render Later: Leveraging @defer for Efficient Data Delivery"
+              event="GraphQL Summit 2023"
+              description="An overview of GraphQL's proposed @defer directive, now available in Apollo Client and GraphOS."
+              href="https://summit.graphql.com/event/c51538f6-4b76-44e3-871e-54180c77cad8/summary"
+              cta="View GraphQL Summit site"
             />
             <Appearance
               title="How to Use Suspense and GraphQL with Apollo to Build Great User Experiences"
-              event="React Advanced London (October 2023)"
+              event="React Advanced London 2023"
               description="The Apollo Client team will show you how we built a non-trivial app using Apollo Client’s new Suspense features and GraphQL features like the @defer directive."
               href="https://reactadvanced.com/"
               cta="View React Advanced site"
             />
           </SpeakingSection>
           <SpeakingSection title="Past">
-            <Appearance
-              href="https://defer-presentation.vercel.app/"
-              title="@defer all the (slow) things"
-              description="An overview of GraphQL's proposed @defer directive, now available in Apollo Client and GraphOS."
-              event="ChicagoJS (March 2023)"
-              cta="View slides"
+          <Appearance
+              title="Suspense + GraphQL = Superpowers"
+              event="React Rally 2023"
+              description="A deep dive into an app built with Apollo Client’s new Suspense hooks and GraphQL features like the @defer directive."
+              href="https://www.youtube.com/live/pxCs9IAhciE?feature=share&t=9858"
+              cta="Watch video"
             />
             <Appearance
               href="https://youtu.be/tq9MfIG-VXw?t=14526"
               title="Next i18n: Performant, Rich Translations in Next.js"
               description="Inter­nation­ali­zation isn't a feature, it’s an architecture."
-              event="JS@PayPal Worldwide Virtual Conference (May 2021)"
+              event="JS@PayPal Worldwide Virtual Conference 2021"
               cta="Watch video"
             />
             <Appearance
               href="https://www.youtube.com/watch?v=6EHbfAmA_0c"
               title="A QuickStart Guide to Making Things on the Internet"
-              // description="A technical deep-dive into HelioStream, the real-time streaming library I wrote for transmitting live video back to Earth."
-              event="Google Women Techmakers Montreal (2016)"
+              event="Google Women Techmakers Montreal 2016"
               cta="Watch video"
             />
           </SpeakingSection>

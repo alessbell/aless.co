0e12fd18282d9bce4b9ea7317f9caa299ffd218c
Alessia Bellisario <github@bellisar.io>
2023-12-04 21:25:06 -0500

    New post: 2023 (#1940)

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,10 @@
 import type { MDXComponents } from 'mdx/types'
- 
+
 // This file allows you to provide custom React components
 // to be used in MDX files. You can import and use any
 // React component you want, including components from
 // other libraries.
- 
+
 // This file is required to use MDX in `app` directory.
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
